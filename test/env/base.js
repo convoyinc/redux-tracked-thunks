231748641d65605e1d232a353f37f945525b3375
Ian MacLeod <ian@nevir.net>
2016-03-28 08:38:08 -0700

    Add chai-subset

--- test/env/base.js
+++ test/env/base.js
@@ -1,4 +1,8 @@
 import * as chai from 'chai';
+import chaiSubset from 'chai-subset';
+
+// Make sure we have a consistent ES2015+ env.
+import 'babel-polyfill';
 
 // Clean up mocha stack traces.
 import 'mocha-clean';
@@ -9,3 +13,11 @@ import 'mocha-clean';
 global.expect = chai.expect;
 // Give us all the info!
 chai.config.truncateThreshold = 0;
+
+// ## http://chaijs.com/plugins/chai-subset
+//
+// Adds object subset assertions.  TL;DR:
+//
+//   * expect(anObject).to.containSubset({abc: 123})
+//
+chai.use(chaiSubset);
